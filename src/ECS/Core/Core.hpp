344c2beac9c33defada607949fe440ef48ee856d
Arthur Spiller <arthur.spiller@epitech.eu>
2024-09-24 13:53:20 +0200

    [+][Feature] Core - Added base of the core developpement architecture. File added: src/ECS/Core/Core.hpp

---
+++ src/ECS/Core/Core.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type - Core : Core
+** File description:
+** Core class is the main class of the ECS.
+** It will coordinate the behaviors of the managers.
+*/
+#pragma once
+
+#include "../Component/ComponentManager/ComponentManager.hpp"
+#include "../System/SystemManager/SystemManager.hpp"
+#include "../Entity/EntityManager/EntityManager.hh"

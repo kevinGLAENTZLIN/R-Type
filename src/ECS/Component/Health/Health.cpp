d0c2afd1645df7de43c3b173dbead5ac69603182
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2024-09-27 15:57:56 +0200

    [+][Doc] add documentation for health component

--- src/ECS/Component/Health/Health.cpp
+++ src/ECS/Component/Health/Health.cpp
@@ -10,7 +10,9 @@
 /**
  * @brief Constructor to health component.
  *
- * Returns void
+ * Constructor for the life component, which will represent the 
+ * life of the entities in the video game, 
+ * with an Integer parameter assigned to the private _health attribute.
  *
  * @tparam Integer to create health component.
  * @return void.
@@ -19,11 +21,23 @@ ECS::Components::Health::Health(int health):
     _health(health)
 {}
 
+/**
+ * @brief Getter _health to health component.
+ *
+ * @tparam void.
+ * @return Integer based on private attribute _health which represent life of an entity.
+ */
 int ECS::Components::Health::getHealth() const
 {
     return _health;
 }
 
+/**
+ * @brief Setter _health to health component.
+ *
+ * @tparam Integer health which represent life of an entity.
+ * @return void.
+ */
 void ECS::Components::Health::setHealth(int health)
 {
     _health = health;

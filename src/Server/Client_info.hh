772daedc282d5e238486362a023e5956333c177b
GabinP-Epitech2027 <114660719+GabinP-Epitech2027@users.noreply.github.com>
2024-10-07 17:10:49 +0200

    Rty 63 server manage client info on the server side (#26)
    * [+][Refactor] Replace the map of client address in the server by a map of client_info (class) with much more info (game room, address, connection, ...)
    File modified :
    src/Server/udp_server.cpp, src/Server/udp_server.hh
    File created :
    src/Server/Client_info.cpp, src/Server/Client_info.hh
    
    * [+][Feat] Set a static class in order to list all possible function of the protocol and their number of parameters. Numbers of parameters are not set yet.
    File created :
    src/Utils/ParametersMap/ParametersMap.hpp
    
    * [+][Feature] Upgrade the Parameter Map to handle the awaited type of each parameter for each functions.
    File modified:
    src/Utils/ParametersMap/ParametersMap.hpp
    
    * [+][Chore & Doc] Add a ParametersMap.cpp to link the map to the Cmake. Also complete the whole documentation of the map.
    File modified:
    src/Utils/ParametersMap/ParametersMap.hpp
    File created:
    src/Utils/ParametersMap/ParametersMap.cpp
    
    * [+][Feature] Implement the history of all commands send to each clients. Also implement the methods to send to the client the command system using the parameters map.
    
    * [+][Doc] Document with Doxygen the whole class and files linked to the Client_info.
    File modified :
    src/Server/Client_info.cpp,
    src/Server/Client_info.hh

---
+++ src/Server/Client_info.hh
@@ -0,0 +1,185 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** client_info
+*/
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdarg>
+#include <map>
+#include <vector>
+#include "../Utils/ParametersMap/ParametersMap.hpp"
+
+/**
+ * @file Client_info.cpp
+ * @brief Declaration of the client info class in order to handle information of the clients in the R-Type Game
+ */
+
+
+namespace Rtype {
+
+    /**
+     * @brief The client_info class represents information about a client in the R-Type game.
+     * 
+     * This class stores various attributes of a client such as their ID, position, address, 
+     * and connection information. It also provides methods to manipulate and retrieve these attributes.
+     */
+    class client_info {
+        public:
+            /**
+             * @brief Default constructor for client_info.
+             */
+            client_info();
+
+            /**
+             * @brief Parameterized constructor for client_info.
+             * 
+             * @param id The unique identifier for the client.
+             * @param port The port number used by the client.
+             * @param addr The IP address of the client.
+             */
+            client_info(int id, int port, std::string addr);
+
+            /**
+             * @brief Destructor for client_info.
+             */
+            ~client_info();
+
+            /**
+             * @brief Gets the unique identifier of the client.
+             * 
+             * @return The client's ID.
+             */
+            int getId() const;
+
+            /**
+             * @brief Sets the unique identifier of the client.
+             * 
+             * @param id The new ID for the client.
+             */
+            void setId(int id);
+
+            /**
+             * @brief Gets the X coordinate of the client.
+             * 
+             * @return The X coordinate.
+             */
+            int getX() const;
+
+            /**
+             * @brief Sets the X coordinate of the client.
+             * 
+             * @param x The new X coordinate.
+             */
+            void setX(int x);
+
+            /**
+             * @brief Gets the Y coordinate of the client.
+             * 
+             * @return The Y coordinate.
+             */
+            int getY() const;
+
+            /**
+             * @brief Sets the Y coordinate of the client.
+             * 
+             * @param y The new Y coordinate.
+             */
+            void setY(int y);
+
+            /**
+             * @brief Gets the expected ACK number for the client.
+             * 
+             * @return The expected ACK number.
+             */
+            int getAckToExpect() const;
+
+            /**
+             * @brief Increments the expected ACK number for the client.
+             */
+            void setAckToExpect();
+
+            /**
+             * @brief Gets the ACK number to be sent to the client.
+             * 
+             * @return The ACK number to be sent.
+             */
+            int getAckToSend() const;
+
+            /**
+             * @brief Increments the ACK number to be sent to the client.
+             */
+            void setAckToSend();
+
+            /**
+             * @brief Gets the game room number the client is in.
+             * 
+             * @return The game room number.
+             */
+            int getRoom() const;
+
+            /**
+             * @brief Sets the game room number for the client.
+             * 
+             * @param room The new game room number.
+             */
+            void setRoom(int room);
+
+            /**
+             * @brief Gets the port number used by the client.
+             * 
+             * @return The client's port number.
+             */
+            int getPort() const;
+
+            /**
+             * @brief Sets the port number for the client.
+             * 
+             * @param port The new port number.
+             */
+            void setPort(int port);
+
+            /**
+             * @brief Gets the IP address of the client.
+             * 
+             * @return The client's IP address.
+             */
+            std::string getAddr() const;
+
+            /**
+             * @brief Sets the IP address of the client.
+             * 
+             * @param address The new IP address.
+             */
+            void setAddr(std::string address);
+
+            /**
+             * @brief Adds a command to the client's command history.
+             * 
+             * This method handle a variadic list of parameters, converts them to strings,
+             * and stores them in the client's history.
+             * 
+             * @param function_type The type of the command.
+             * @param function_index The index of the command.
+             * @param params The variadic parameters for the command.
+             */
+            void pushCmdToHistory(int function_type, int function_index, std::va_list params);
+
+        private:
+            int _id;
+            int _x;
+            int _y;
+            int _AckExpected;
+            int _AckToSend;
+            int _gameRoom;
+            bool _inGame;
+            bool _isAlive;
+            int _port;
+            std::string _addr;
+            std::map<int, std::tuple<int, int,  std::vector<std::string>>> _history;
+    };
+}
---
+++ src/Utils/ParametersMap/ParametersMap.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** ParametersMap
+*/
+
+#include "ParametersMap.hpp"
+
+namespace Utils {
+    // Define the static member variable of the ParametersMap class.
+    std::map<std::pair<int, int>, std::pair<std::string, std::string>> ParametersMap::_parametersMap;
+}

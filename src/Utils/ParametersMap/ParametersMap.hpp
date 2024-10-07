8faf304ddca1c6f3d2c1050d93cec427c93f2564
Gabin <gabin.pinos@epitech.eu>
2024-10-07 15:36:42 +0200

    [+][Chore & Doc] Add a ParametersMap.cpp to link the map to the Cmake. Also complete the whole documentation of the map. File modified: src/Utils/ParametersMap/ParametersMap.hpp File created: src/Utils/ParametersMap/ParametersMap.cpp

--- src/Utils/ParametersMap/ParametersMap.hpp
+++ src/Utils/ParametersMap/ParametersMap.hpp
@@ -12,6 +12,7 @@
 
 namespace Utils
 {
+    // Enums representing different categories of functions.
     enum InfoTypeEnum {
         GameInfo = 0,
         Player,
@@ -21,6 +22,7 @@ namespace Utils
         Projectile,
     };
 
+    // Enums for various game-related functions.
     enum GameInfoEnum {
         NewClientConnected = 0,
         GamesAvailable,
@@ -31,6 +33,7 @@ namespace Utils
         LevelComplete
     };
 
+    // Enums for player-related functions.
     enum PlayerEnum {
         PlayerSpawnOnGame = 0,
         PlayerDie,
@@ -41,6 +44,7 @@ namespace Utils
         PlayerScore
     };
 
+    // Enums for enemy-related functions.
     enum EnemyEnum {
         EnemySpawn = 0,
         EnemyDie,
@@ -48,25 +52,35 @@ namespace Utils
         EnemyAttack
     };
 
+    // Enums for boss-related functions.
     enum BossEnum {
         BossSpawn = 0,
         BossDie,
         BossAttack
     };
 
+    // Enums for power-up-related functions.
     enum PowerUpEnum {
         PowerUpSpawn = 0,
         PowerUpDisappear,
         PowerUpAttack
     };
 
+    // Enums for projectile-related functions.
     enum ProjectileEnum {
         ProjectileFired = 0,
         ProjectileHit
     };
 
+    // Class managing a mapping of parameters for all functions of the protocol.
     class ParametersMap {
         public:
+            /**
+             * @brief Initialize the parameters map with all functions and their parameters.
+             * 
+             * Sets up the mapping of function types and indexes to their corresponding 
+             * parameter types expected by the server and client.
+             */
             static void init_map() 
             {
                 _parametersMap = {
@@ -104,35 +118,59 @@ namespace Utils
                 };
             }
 
+            /**
+             * @brief Get the number of parameters on the server side.
+             * @param function_type The category of the function.
+             * @param function_index The index of the function.
+             * @return The number of parameters expected by the server.
+             */
             static int getNbParameterPerFunctionServer(int function_type, int function_index)
             {
                 return _parametersMap[{function_type, function_index}].first.length();
             }
 
+            /**
+             * @brief Get the number of parameters on the client side.
+             * @param function_type The category of the function.
+             * @param function_index The index of the function.
+             * @return The number of parameters expected by the client.
+             */
             static int getNbParameterPerFunctionClient(int function_type, int function_index)
             {
                 return _parametersMap[{function_type, function_index}].second.length();
             }
 
+            /**
+             * @brief Get the parameter types for a server function.
+             * @param function_type The category of the function.
+             * @param function_index The index of the function.
+             * @return The parameter types expected by the server as a string.
+             */
             static std::string getParameterTypePerFunctionServer(int function_type, int function_index)
             {
                 return _parametersMap[{function_type, function_index}].first;
             }
 
+            /**
+             * @brief Get the parameter types for a client function.
+             * @param function_type The category of the function.
+             * @param function_index The index of the function.
+             * @return The parameter types expected by the client as a string.
+             */
             static std::string getParameterTypePerFunctionClient(int function_type, int function_index)
             {
                 return _parametersMap[{function_type, function_index}].second;
             }
 
-
         private:
-            // Correspond to the map of all functions available with the protocol.
-            //
-            // The first pair correspond to the key : function type & function index.
-            // Function type and index are set in some Enums
-            //
-            // The second pair correspond to the type of parameter. (b: Bool, c: Char, i: Int, f: Float)
-            // The first string is about parameters awaited on the server side, the second one is by the client side.
+            /**
+             * @brief Map of function caategories and indexes to parameter types.
+             * 
+             * The map key is a pair consisting of the function category & index, 
+             * while the value is a pair of strings representing the parameter types 
+             * expected by the server & client, respectively.
+             * Parameter types are denoted by characters (e.g., b: Bool, c: Char, i: Int, f: Float).
+             */
             static std::map<std::pair<int, int>, std::pair<std::string, std::string>> _parametersMap; 
     };
 }

0661f0feecc67bea373e747e0867226f9186c46e
Arthur Spiller <arthur.spiller@epitech.eu>
2024-10-09 17:13:42 +0200

    [+][Feature] - ECS: Added UpdateVelocityPataPata system. Added setters for the Velocity component. File modified: src/ECS/Component/Velocity/Velocity.cpp, src/ECS/Component/Velocity/Velocity.hh File created: src/ECS/System/UpdateVelocityPataPata/UpdateVelocityPataPata.cpp, src/ECS/System/UpdateVelocityPataPata/UpdateVelocityPataPata.hh

---
+++ src/ECS/System/UpdateVelocityPataPata/UpdateVelocityPataPata.hh
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../System/System.hpp"
+
+#include "../../Component/ComponentManager/SparseArray.hpp"
+#include "../../Component/Velocity/Velocity.hh"
+
+namespace ECS {
+    namespace Systems {
+
+        class UpdateVelocityPataPata : public System {
+        public:
+            UpdateVelocityPataPata() = default;
+            ~UpdateVelocityPataPata() = default;
+
+            void update(
+                ECS::ComponentManager::SparseArray<ECS::Components::Velocity> & velocities,
+                std::vector<std::size_t> pataPatas);
+        };
+    }
+}

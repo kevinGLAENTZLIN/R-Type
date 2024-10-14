63ba0e67576033280ac2345fe29f4a3a5e1146ba
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2024-10-14 16:48:16 +0200

    [+][Doc] add documentation for InputUpdate

--- src/ECS/System/InputUpdates/InputUpdates.hh
+++ src/ECS/System/InputUpdates/InputUpdates.hh
@@ -16,20 +16,55 @@
 namespace ECS {
     namespace Systems {
 
+        /**
+        * @brief System for handling updates based on user inputs.
+        *
+        * The InputUpdates system is responsible for updating components of entities based on the
+        * inputs provided.
+        */
         class InputUpdates : public System{
         public:
+            /**
+            * @brief Default constructor for InputUpdates.
+            */
             InputUpdates() = default;
+
+            /**
+            * @brief Default destructor for InputUpdates.
+            */
             ~InputUpdates() = default;
 
+            /**
+            * @brief Updates the input components of entities based on provided inputs.
+            *
+            * This method processes the input components of entities and updates them according
+            * to the user input values. It checks the input array and associates them with
+            * the respective entities.
+            *
+            * @param inputs Vector of input entity IDs to process.
+            * @param inputComponents SparseArray containing the Input components of the entities.
+            * @param entities Vector of entity IDs whose input components need to be updated.
+            * @return The ID of the entity whose inputs were successfully updated.
+            */
             std::size_t updateInputs(const std::vector<std::size_t> & inputs,
                               ECS::ComponentManager::SparseArray<ECS::Components::Input> & inputComponents,
                               std::vector<std::size_t> entities);
 
+            /**
+            * @brief Updates the velocity of entities based on their input components.
+            *
+            * This method updates the velocity components of entities based on their respective
+            * input components.
+            *
+            * @param inputs SparseArray containing the Input components of entities.
+            * @param velocities SparseArray containing the Velocity components of entities.
+            * @param entities Vector of entity IDs to update the velocity for.
+            * @return void.
+            */
             void updateInputedVelocity(
                 ECS::ComponentManager::SparseArray<ECS::Components::Input> & inputs,
                 ECS::ComponentManager::SparseArray<ECS::Components::Velocity> & velocitites,
                 std::vector<std::size_t> entities);
-
 
         };
     }

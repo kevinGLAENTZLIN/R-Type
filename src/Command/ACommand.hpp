6634a77ea37aadcf6a5f551aaeadaab8dd1eb5ff
Evan CANDAN <114905170+evancandan@users.noreply.github.com>
2024-10-16 02:18:01 +0200

    [~][Temp-Fix] compile the ParameterMap directly in the Client for solving the compilation issue

--- src/Command/ACommand.hpp
+++ src/Command/ACommand.hpp
@@ -60,7 +60,7 @@ namespace Rtype
                         va_list params;
                         va_list params_copy;
                         Utils::Network::bytes msg;
-                        // std::string params_type = getParamsType(function_type, function_index);
+                        std::string params_type = getParamsType(function_type, function_index);
 
                         va_start(params, function_index);
                         va_copy(params_copy, params);
@@ -69,7 +69,7 @@ namespace Rtype
                             // pushCmdToHistory(function_type, function_index, params);
                         else
                             std::cerr << "Warning, the history has not been set" << std::endl;
-                        msg = Utils::Network::Protocol::CreateMsg(_ack, function_type, function_index, Utils::Network::Protocol::va_listToVector(params_copy, ""));
+                        msg = Utils::Network::Protocol::CreateMsg(_ack, function_type, function_index, Utils::Network::Protocol::va_listToVector(params_copy, params_type));
                         va_end(params);
                         _senderSocket->async_send_to(boost::asio::buffer(msg), _endpoint,
                         [this] (boost::system::error_code ec, std::size_t recvd_bytes) {

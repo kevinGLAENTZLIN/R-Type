d0d48e67a7ec3535ab8fbcea0c08ddbcfc70d73c
Gabin <gabin.pinos@epitech.eu>
2024-11-02 15:18:08 +0100

    [+]Feat] Error handling on server & clients params

--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -7,9 +7,25 @@
 
 #include "udp_server.hpp"
 
+bool is_number(std::string str)
+{
+    return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+bool isValidPort(std::string str)
+{
+    int port = 0;
+
+    if (is_number(str)) {
+        port = std::stoi(str);
+        return (port > 0 && port < 10000);
+    }
+    return false;
+}
+
 int main(int argc, char* argv[])
 {
-    if (argc != 2) {
+    if (argc != 2 || !isValidPort(argv[1])) {
         std::cerr << "Usage: udp_server <port>\n";
         return 1;
     }
